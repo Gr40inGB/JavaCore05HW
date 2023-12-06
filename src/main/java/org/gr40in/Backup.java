60e90045d3196c4f97b3f68ccbe935c38c2f7b26
Rishat_Imangulov <gr40in@yandex.ru>
2023-12-07 03:22:07 +0500

    all done

--- src/main/java/org/gr40in/Backup.java
+++ src/main/java/org/gr40in/Backup.java
@@ -1,19 +1,44 @@
 package org.gr40in;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Backup {
+
     public static void main(String[] args) throws IOException {
-        makeBackup(Path.of("."));
+
+        // go up and create backup folder. And into him <backup + Date>
+        Path backupFolder = Path.of("..//backup//backup" +
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("-yyyy-MM-dd-hh-mm")));
+        makeBackup(Path.of("."), backupFolder);
     }
 
-    public static void makeBackup(Path path) throws IOException {
-        DirectoryStream directoryStream = Files.newDirectoryStream(path);
+    public static void makeBackup(Path source, Path destination) throws IOException {
+
+        DirectoryStream<Path> directoryStream =
+                Files.newDirectoryStream(source);  //"[!back]*"); glob ignored Backup.java =(
+
         for (var f : directoryStream) {
-            System.out.println(f);
+            Path newDestination = Path.of(destination.toString() + f.toString());
+            if (Files.isDirectory(f)) {
+                Files.createDirectories(newDestination);
+                makeBackup(f, destination);
+            } else //System.out.println(f);
+                copyFile(f, destination);
+        }
+    }
+
+    public static void copyFile(Path file, Path destination) throws IOException {
+        FileInputStream inputStream = new FileInputStream(file.toFile());
+        FileOutputStream fileOutputStream = new FileOutputStream(Path.of(destination.toString() + file).toFile());
+        int count;
+        byte[] buffer = new byte[1024];
+        while ((count = inputStream.read(buffer)) != -1) {
+            fileOutputStream.write(buffer, 0, count);
         }
     }
 
